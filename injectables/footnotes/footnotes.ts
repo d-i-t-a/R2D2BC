8e2e7b7d8a15bc7fb604ac816aad6ccbf426aaa4
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-10-19 13:10:22 +0200

    updated copyright header

--- injectables/footnotes/footnotes.ts
+++ injectables/footnotes/footnotes.ts
@@ -1,7 +1,7 @@
 /*
  * Project: R2D2BC - Web Reader
  * Developers: Aferdita Muriqi
- * Copyright (c) 2019. Aferdita Muriqi. All rights reserved.
+ * Copyright (c) 2019. DITA. All rights reserved.
  * Developed on behalf of: CAST (http://www.cast.org)
  * Licensed to: Bokbasen AS and CAST under one or more contributor license agreements.
  * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
--- src/model/Locator.ts
+++ src/model/Locator.ts
@@ -2,7 +2,7 @@
 /*
  * Project: R2D2BC - Web Reader
  * Developers: Aferdita Muriqi
- * Copyright (c) 2019. Aferdita Muriqi. All rights reserved.
+ * Copyright (c) 2019. DITA. All rights reserved.
  * Developed on behalf of: Bokbasen AS (https://www.bokbasen.no)
  * Licensed to: Bokbasen AS and CAST under one or more contributor license agreements.
  * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.

6654d2ee7194d8eee487d5ff77bc58c37d7a2c13
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2020-01-28 08:36:43 -0500

    cleanup event handler

--- src/utils/EventHandler.ts
+++ src/utils/EventHandler.ts
@@ -7,8 +7,6 @@
  * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
  */
 
-import * as BrowserUtilities from "./BrowserUtilities";
-
 
 export function addEventListenerOptional(element: any, eventType: string, eventListener: any) {
     if (element) {
@@ -23,43 +21,12 @@ export function removeEventListenerOptional(element: any, eventType: string, eve
 
 
 export default class EventHandler {
-    private pendingMouseEventStart: MouseEvent | null = null;
-    private pendingMouseEventEnd: MouseEvent | null = null;
-    private pendingTouchEventStart: TouchEvent | null = null;
-    private pendingTouchEventEnd: TouchEvent | null = null;
-
-    public onLeftTap: (event: UIEvent) => void = () => {};
-    public onMiddleTap: (event: UIEvent) => void = () => {};
-    public onRightTap: (event: UIEvent) => void = () => {};
-    public onBackwardSwipe: (event: UIEvent) => void = () => {};
-    public onForwardSwipe: (event: UIEvent) => void = () => {};
-
-    public onLeftArrow: (event: UIEvent) => void = () => {};
-    public onRightArrow: (event: UIEvent) => void = () => {};
-
-    public onLeftHover: () => void = () => {};
-    public onRightHover: () => void = () => {};
-    public onRemoveHover: () => void = () => {};
 
     public onInternalLink: (event: UIEvent) => void = () => {};
-
-    private static readonly CLICK_PIXEL_TOLERANCE = 10;
-    private static readonly TAP_PIXEL_TOLERANCE = 10;
-    private static readonly DOUBLE_CLICK_MS = 200;
-    private static readonly LONG_PRESS_MS = 500;
-    private static readonly DOUBLE_TAP_MS = 200;
-    private static readonly SLOW_SWIPE_MS = 500;
+    public onClickThrough: (event: UIEvent) => void = () => {};
 
     public setupEvents(element: HTMLElement | Document | null) {
         if (element !== null) {
-            element.addEventListener("touchstart", this.handleTouchEventStart.bind(this));
-            element.addEventListener("touchend", this.handleTouchEventEnd.bind(this));
-            element.addEventListener("mousedown", this.handleMouseEventStart.bind(this));
-            element.addEventListener("mouseup", this.handleMouseEventEnd.bind(this));
-            element.addEventListener("mouseenter", this.handleMouseMove.bind(this));
-            element.addEventListener("mousemove", this.handleMouseMove.bind(this));
-            element.addEventListener("mouseleave", this.handleMouseLeave.bind(this));
-
             // Most click handling is done in the touchend and mouseup event handlers,
             // but if there's a click on an external link we need to cancel the click
             // event to prevent it from opening in the iframe.
@@ -68,195 +35,6 @@ export default class EventHandler {
         } else {
             throw "cannot setup events for null";
         }
-    }
-
-    private handleMouseEventStart = (event: MouseEvent): void => {
-        this.pendingMouseEventStart = event;
-    }
-
-    private handleTouchEventStart = (event: TouchEvent): void => {
-        if (BrowserUtilities.isZoomed()) {
-            return;
-        }
-
-        if (event.changedTouches.length !== 1) {
-            // This is a multi-touch event. Ignore.
-            return;
-        }
-
-        this.pendingTouchEventStart = event;
-    }
-
-    private handleMouseEventEnd = (event: MouseEvent): void => {
-        if (!this.pendingMouseEventStart) {
-            // Somehow we got an end event without a start event. Ignore it.
-            return;
-        }
-
-        const devicePixelRatio = window.devicePixelRatio;
-
-        const xDevicePixels = (this.pendingMouseEventStart.clientX - event.clientX) / devicePixelRatio;
-        const yDevicePixels = (this.pendingMouseEventStart.clientY - event.clientY) / devicePixelRatio;
-
-        // Is the end event in the same place as the start event?
-        if (Math.abs(xDevicePixels) < EventHandler.CLICK_PIXEL_TOLERANCE && Math.abs(yDevicePixels) < EventHandler.CLICK_PIXEL_TOLERANCE) {
-            if (this.pendingMouseEventEnd) {
-                // This was a double click. Let the browser handle it.
-                this.pendingMouseEventStart = null;
-                this.pendingMouseEventEnd = null;
-                return;
-            }
-
-            // This was a single click.
-            this.pendingMouseEventStart = null;
-            this.pendingMouseEventEnd = event;
-            setTimeout(this.handleClick, EventHandler.DOUBLE_CLICK_MS);
-            return;
-        }
-
-        this.pendingMouseEventEnd = null;
-
-        // This is a swipe or highlight. Let the browser handle it.
-        // (Swipes aren't handled on desktop.)
-        this.pendingMouseEventStart = null;
-    }
-
-    private handleTouchEventEnd = (event: TouchEvent): void => {
-        event.preventDefault();
-        if (BrowserUtilities.isZoomed()) {
-            return;
-        }
-
-        if (event.changedTouches.length !== 1) {
-            // This is a multi-touch event. Ignore.
-            return;
-        }
-
-        if (!this.pendingTouchEventStart) {
-            // Somehow we got an end event without a start event. Ignore it.
-            return;
-        }
-
-        const startTouch = this.pendingTouchEventStart.changedTouches[0];
-        const endTouch = event.changedTouches[0];
-
-        if (!startTouch) {
-            // Somehow we saved a touch event with no touches.
-            return;
-        }
-
-        const devicePixelRatio = window.devicePixelRatio;
-
-        const xDevicePixels = (startTouch.clientX - endTouch.clientX) / devicePixelRatio;
-        const yDevicePixels = (startTouch.clientY - endTouch.clientY) / devicePixelRatio;
-
-        // Is the end event in the same place as the start event?
-        if (Math.abs(xDevicePixels) < EventHandler.TAP_PIXEL_TOLERANCE && Math.abs(yDevicePixels) < EventHandler.TAP_PIXEL_TOLERANCE) {
-            if (this.pendingTouchEventEnd) {
-                // This was a double tap. Let the browser handle it.
-                this.pendingTouchEventStart = null;
-                this.pendingTouchEventEnd = null;
-                return;
-            }
-
-            // This was a single tap or long press.
-            if (event.timeStamp - this.pendingTouchEventStart.timeStamp > EventHandler.LONG_PRESS_MS) {
-                // This was a long press. Let the browser handle it.
-                this.pendingTouchEventStart = null;
-                this.pendingTouchEventEnd = null;
-                return;
-            }
-
-            // This was a single tap.
-            this.pendingTouchEventStart = null;
-            this.pendingTouchEventEnd = event;
-            setTimeout(this.handleTap, EventHandler.DOUBLE_TAP_MS);
-            return;
-        }
-
-        this.pendingTouchEventEnd = null;
-
-        if (event.timeStamp - this.pendingTouchEventStart.timeStamp > EventHandler.SLOW_SWIPE_MS) {
-            // This is a slow swipe / highlight. Let the browser handle it.
-            this.pendingTouchEventStart = null;
-            return;
-        }
-
-        // This is a swipe. 
-        const slope = (startTouch.clientY - endTouch.clientY) / (startTouch.clientX - endTouch.clientX);
-        if (Math.abs(slope) > 0.5) {
-            // This is a mostly vertical swipe. Ignore.
-            this.pendingTouchEventStart = null;
-            return;
-        }
-
-        // This was a horizontal swipe.
-        if (xDevicePixels < 0) {
-            this.onBackwardSwipe(event);
-        } else {
-            this.onForwardSwipe(event);
-        }
-        this.pendingTouchEventStart = null;
-    }
-
-    private handleClick = (): void => {
-        if (!this.pendingMouseEventEnd) {
-            // Another click happened already.
-            return;
-        }
-
-        if (this.checkForLink(this.pendingMouseEventEnd)) {
-            // This was a single click on a link. Do nothing.
-            this.pendingMouseEventEnd = null;
-            return;
-        }
-
-        // This was a single click.
-        const x = this.pendingMouseEventEnd.clientX;
-        const width = window.innerWidth;
-        if (x / width < 0.05) {
-            this.onLeftTap(this.pendingMouseEventEnd);
-        } else if (x / width > 0.95) {
-            this.onRightTap(this.pendingMouseEventEnd);
-        } else {
-            this.onMiddleTap(this.pendingMouseEventEnd);
-        }
-        this.pendingMouseEventEnd = null;
-        return;
-    }
-
-    private handleTap = (): void => {
-        if (!this.pendingTouchEventEnd) {
-            // Another tap happened already.
-            return;
-        }
-
-        if (this.checkForLink(this.pendingTouchEventEnd)) {
-            this.handleLinks(this.pendingTouchEventEnd);
-            
-            // This was a single tap on a link. Do nothing.
-            this.pendingTouchEventEnd = null;
-            return;
-        }
-
-        // This was a single tap.
-        const touch = this.pendingTouchEventEnd.changedTouches[0];
-        if (!touch) {
-            // Somehow we got a touch event with no touches.
-            return;
-        }
-
-        const x = touch.clientX;
-        const width = window.innerWidth;
-        if (x / width < 0.05) {
-            this.onLeftTap(this.pendingTouchEventEnd);
-        } else if (x / width > 0.95) {
-            this.onRightTap(this.pendingTouchEventEnd);
-        } else {
-            this.onMiddleTap(this.pendingTouchEventEnd);
-        }
-        this.pendingTouchEventEnd = null;
-        return;
     }
 
     private checkForLink = (event: MouseEvent | TouchEvent): HTMLAnchorElement | null => {
@@ -271,23 +49,10 @@ export default class EventHandler {
         return null;
     }
 
-    private handleMouseMove = (event: MouseEvent): void => {
-        const x = event.clientX;
-        const width = window.innerWidth;
-        if (x / width < 0.05) {
-            this.onLeftHover();
-        } else if (x / width > 0.95) {
-            this.onRightHover();
-        } else {
-            this.onRemoveHover();
-        }
-    }
-
-    private handleMouseLeave = (): void => {
-        this.onRemoveHover();
-    }
 
     private handleLinks = (event: MouseEvent | TouchEvent): void => {
+        console.log("R2 Click Handler")
+
         const link = this.checkForLink(event);
         if (link) {
             // Open external links in new tabs.
@@ -310,7 +75,9 @@ export default class EventHandler {
                     // link.click();
                     this.onInternalLink(event);
                 }
-         }
+            }
+        } else {
+            this.onClickThrough(event);
         }
     }
 

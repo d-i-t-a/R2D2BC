cf0abdf46aa14afd391f83543b6a0dee4fa697b0
Paul Swanson <swandog30@gmail.com>
2020-07-06 19:39:00 -0500

    Cleaned up license header

--- injectables/click/click.ts
+++ injectables/click/click.ts
@@ -15,7 +15,6 @@
  *
  * Developed on behalf of: CAST (http://www.cast.org)
  * Licensed to: Bokbasen AS and CAST under one or more contributor license agreements.
- * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
  */
 
 export const IS_DEV = (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "dev");
--- src/navigator/IFrameNavigator.ts
+++ src/navigator/IFrameNavigator.ts
@@ -15,7 +15,6 @@
  *
  * Developed on behalf of: Bokbasen AS (https://www.bokbasen.no), CAST (http://www.cast.org)
  * Licensed to: Bokbasen AS and CAST under one or more contributor license agreements.
- * Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
  */
 
 import Navigator from "./Navigator";

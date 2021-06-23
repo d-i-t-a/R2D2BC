2daf597203a53b57102e37c1bc8bc29668bbc124
kristojorg <kristo.jorgenson@gmail.com>
2021-06-23 18:18:15 +0200

    fix react example css

--- examples/react/index.tsx
+++ examples/react/index.tsx
@@ -3,6 +3,10 @@ import "regenerator-runtime/runtime";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import D2Reader from "../../src";
+// import "../../src/styles/sass/reader.scss";
+import readiumBefore from "url:./readium-css/ReadiumCSS-before.css";
+import readiumAfter from "url:./readium-css/ReadiumCSS-after.css";
+import readiumDefault from "url:./readium-css/ReadiumCSS-default.css";
 
 const App = () => {
   const [reader, setReader] = React.useState<D2Reader | null>(null);
@@ -12,32 +16,8 @@ const App = () => {
     D2Reader.build({
       url,
       injectables: injectables as any,
-
-      // all of these were required
-      userSettings: {
-        verticalScroll: "readium-scroll-off",
-      },
-      initialAnnotations: undefined,
-      lastReadingPosition: undefined,
-      upLinkUrl: undefined,
-      material: {
-        settings: {
-          fontOverride: false,
-          advancedSettings: false,
-          pageMargins: false,
-          lineHeight: false,
-        },
-      },
-      rights: {
-        autoGeneratePositions: false,
-      },
-      tts: undefined,
-      search: { color: "red", current: "blah" },
-      annotations: { initialAnnotationColor: "blue" },
-      highlighter: { selectionMenuItems: [] },
-      useLocalStorage: false,
-      attributes: { margin: 2 },
-    } as any).then(setReader);
+      injectablesFixed: [],
+    }).then(setReader);
   }, []);
 
   const isScrolling = reader?.currentSettings.verticalScroll ?? false;
@@ -88,59 +68,17 @@ ReactDOM.render(<App />, document.getElementById("root"));
 const injectables = [
   {
     type: "style",
-    url: "http://localhost:1234/viewer/readium-css/ReadiumCSS-before.css",
+    url: readiumBefore,
     r2before: true,
   },
   {
     type: "style",
-    url: "http://localhost:1234/viewer/readium-css/ReadiumCSS-default.css",
+    url: readiumDefault,
     r2default: true,
   },
   {
     type: "style",
-    url: "http://localhost:1234/viewer/readium-css/ReadiumCSS-after.css",
+    url: readiumAfter,
     r2after: true,
-  },
-  {
-    type: "style",
-    url: "http://localhost:1234/viewer/injectables/pagebreak/pagebreak.css",
-    r2after: true,
-  },
-  // { type: 'style', url: 'http://localhost:1234/viewer/readium-css/neon-after.css', r2after: true, appearance: 'neon' },
-  {
-    type: "script",
-    url:
-      "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML&latest",
-  },
-  {
-    type: "style",
-    url: "http://localhost:1234/viewer/fonts/opendyslexic/opendyslexic.css",
-    fontFamily: "opendyslexic",
-    systemFont: false,
-  },
-  { type: "style", fontFamily: "Courier", systemFont: true },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/click/click.js',
-  // },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/footnotes/footnotes.js',
-  // },
-  {
-    type: "style",
-    url: "http://localhost:1234/viewer/injectables/footnotes/footnotes.css",
-  },
-  // {
-  //   type: 'script',
-  //   url: 'http://localhost:1234/viewer/injectables/glossary/glossary.js',
-  // },
-  {
-    type: "style",
-    url: "http://localhost:1234/viewer/injectables/glossary/glossary.css",
-  },
-  {
-    type: "style",
-    url: "http://localhost:1234/viewer/injectables/style/style.css",
   },
 ];
